8c33bee8899da1a4b2d47fd42eac6b2c9b4c5f86
Victor Ironali <victor@jobin.cloud>
2025-10-18 16:54:40 +0100

    Links updated, Graph meta tags updated too

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const mobileMenuIcon = document.getElementById('mobileMenuIcon');
     const mobileDropdown = document.querySelector('.mobile-dropdown');
@@ -10,5 +9,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     closeIcon.addEventListener('click', () => {
         mobileDropdown.classList.remove('active');
+    });
+
+    const sections = document.querySelectorAll('.section-fade');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
     });
 });
